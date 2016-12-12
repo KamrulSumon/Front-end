e08ba1c7f9d68c455a79ad0378a3b8f61d24f4d3
Kamrul Hasan <hasanm@goldmail.etsu.edu>
2016-12-12 13:05:36 -0500

    AngularJS: Voting application

---
+++ AngularJS/AngularJS-Projects/VoteGuru/public/controllers/midbarController.js
@@ -0,0 +1,34 @@
+
+(function (){
+    'use strict';
+
+    angular
+        .module('plunker')
+        .controller('midbarController', midbarController);
+
+
+    function midbarController($state) {
+
+
+        var vm = this;
+
+        vm.activate = activate;
+        vm.signup = signup;
+
+        function signup(){
+            $state.go('signup');
+        }
+
+        activate();
+
+        function activate(){
+            console.log("midbar controller activated");
+
+        }
+    }
+
+})();
+
+
+
+
---
+++ AngularJS/AngularJS-Projects/VoteGuru/public/controllers/navbarController.js
@@ -0,0 +1,43 @@
+
+(function (){
+    'use strict';
+
+    angular
+        .module('plunker')
+        .controller('navbarController', navbarController);
+
+
+    function navbarController(voteGuruService, navbarService) {
+
+
+        var vm = this;
+
+        vm.activate = activate;
+
+        vm.user = {};
+        vm.showSettingsOrLogout = showSettingsOrLogout;
+        vm.showLogin = showLogin;
+        vm.showSignup = showSignup;
+
+        activate();
+
+        function activate(){
+            console.log("navbar controller activated");
+            vm.user = voteGuruService.getUser();
+            // just getting user info
+        }
+
+        function showLogin() {
+            return navbarService.showLogin();
+        }
+
+        function showSignup() {
+            return navbarService.showSignup();
+        }
+
+        function showSettingsOrLogout() {
+            return navbarService.showSettingsOrLogout();
+        }
+    }
+
+})();
